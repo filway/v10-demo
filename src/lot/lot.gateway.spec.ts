5f858cd58ae558a77d19d651826ea58c13aaaba3
zhouxiao <zhouxiao@npn.sg>
2024-06-06 11:02:22 +0800

    init

---
+++ src/lot/lot.gateway.spec.ts
@@ -0,0 +1,54 @@
+import { Test } from '@nestjs/testing';
+import { LotGateway } from './lot.gateway';
+import { INestApplication } from '@nestjs/common';
+import { Socket, io } from 'socket.io-client';
+
+async function createNestApp(...gateways: any): Promise<INestApplication> {
+  const testingModule = await Test.createTestingModule({
+    providers: gateways,
+  }).compile();
+  return testingModule.createNestApplication();
+}
+
+describe('LotGateway', () => {
+  let gateway: LotGateway;
+  let app: INestApplication;
+  let ioClient: Socket;
+
+  beforeAll(async () => {
+    // Instantiate the app
+    app = await createNestApp(LotGateway);
+    // Get the gateway instance from the app instance
+    gateway = app.get<LotGateway>(LotGateway);
+    // Create a new client that will interact with the gateway
+    ioClient = io('http://localhost:3000', {
+      autoConnect: false,
+      transports: ['websocket', 'polling'],
+    });
+
+    app.listen(3000);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be defined', () => {
+    expect(gateway).toBeDefined();
+  });
+
+  it('should emit "pong" on "ping"', async () => {
+    ioClient.connect();
+    ioClient.emit('ping', 'Hello world!');
+    await new Promise<void>((resolve) => {
+      ioClient.on('connect', () => {
+        console.log('connected');
+      });
+      ioClient.on('pong', (data) => {
+        expect(data).toBe('Hello world!');
+        resolve();
+      });
+    });
+    ioClient.disconnect();
+  });
+});
